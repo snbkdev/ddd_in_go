1f48f6d3c3517052d65188f420b27fb8a83052d5
Asanbek09 <samudinua@gmail.com>
2025-12-15 11:21:26 +0600

    Начало изучения DDD

---
+++ chapter_02/leads.go
@@ -0,0 +1,36 @@
+package chapter02
+
+import "context"
+
+type LeadRequest struct {
+	email string
+}
+
+type Lead struct {
+	id string
+}
+
+type LeadCreator interface {
+	CreateLead(ctx context.Context, request LeadRequest) (Lead, error)
+}
+
+type Customer struct {
+	leadID string
+	userID string
+}
+
+func (c *Customer) UserID() string {
+	return c.userID
+}
+
+func (c *Customer) SetUserID(userID string) {
+	c.userID = userID
+}
+
+type LeadConvertor interface {
+	Convert(ctx context.Context, subSelection subscriptionType) (Customer, error)
+}
+
+func (l Lead) Convert(ctx context.Context, subSelection subscriptionType) (Customer, error) {
+	panic("implement me")
+}
