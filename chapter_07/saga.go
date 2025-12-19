abb662b9c782719266e60576179598b67bb40f92
Asanbek09 <samudinua@gmail.com>
2025-12-19 16:54:54 +0600

    Простенький пример к главе 7

---
+++ chapter_07/saga.go
@@ -0,0 +1,51 @@
+package chapter07
+
+import "context"
+
+type Saga interface {
+	Execute(ctx context.Context) error
+	Rollback(ctx context.Context) error
+}
+
+type OrderCreator struct{}
+
+func (o OrderCreator) Execute(ctx context.Context) error {
+	return o.createOrder(ctx)
+}
+
+func (o OrderCreator) Rollback(ctx context.Context) error {
+	return nil
+}
+func (o OrderCreator) createOrder(ctx context.Context) error {
+	return nil
+}
+
+type PaymentCreator struct{}
+
+func (p PaymentCreator) Execute(ctx context.Context) error {
+	return p.createPayment(ctx)
+}
+
+func (p PaymentCreator) Rollback(ctx context.Context) error {
+	return nil
+}
+
+func (p PaymentCreator) createPayment(ctx context.Context) error {
+	return nil
+}
+
+type SagaManager struct {
+	actions []Saga
+}
+
+func (s SagaManager) Handle(ctx context.Context) {
+	for i, action := range s.actions {
+		if err := action.Execute(ctx); err != nil {
+			for j := 0; j < i; j++ {
+				if err := s.actions[j].Rollback(ctx); err != nil {
+					//
+				}
+			}
+		}
+	}
+}
